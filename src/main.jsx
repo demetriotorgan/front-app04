ae7c9986f9e17d046ecce6533e91e3bfa305d6e9
demetriotorgan <demetriotorgan@hotmail.com>
2024-12-13 14:58:37 -0300

    configuracoesAPP

--- src/main.jsx
+++ src/main.jsx
@@ -46,6 +46,11 @@ const router = createBrowserRouter([
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router} />
+   <RouterProvider 
+    router={router} 
+    future={{
+      v7_startTransition:true,
+    }}
+   />
   </StrictMode>,
 )
